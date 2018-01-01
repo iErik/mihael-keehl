8ce7f300d9dd965a62ce3386bc1319bbae079e99
Erik Isidore <ericssonio@protonmail.ch>
2018-01-01 18:57:01 -0200

    Display tags in content-single template

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,15 +1,20 @@
+<?php use Roots\Sage\Tags; ?>
+
 <article <?php post_class(); ?>>
   <header class="post-heading">
     <div class="post-title">
-      <h1 class="title h4"><?php the_title(); ?></h1>
+      <h1 class="title h4">
+        <?php the_title(); ?>
+      </h1>
     </div>
+    
     <?php if ( is_single() ) : ?>
       <div class="post-meta">
-        <time class="post-time" datetime="<?= get_post_time('c', true); ?>" >
-          <?= get_the_date(); ?>
+        <time class="post-time" datetime="<?php echo get_post_time('c', true); ?>" >
+          <?php echo get_the_date(); ?>
         </time>
-        <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="post-author">
-          <?= get_the_author(); ?>
+        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="post-author">
+          <?php echo get_the_author(); ?>
         </a>
       </div>
     <?php endif; ?>
@@ -18,6 +23,11 @@
   <div class="post-content">
     <?php the_content(); ?>
   </div>
+
+  <footer class="post-footer">
+    <?php Tags\mihael_post_tags(); ?>
+    <?php Tags\mihael_post_pagination(); ?>
+  </footer>
 </article>
 
 <?php if (comments_open() || get_comments_number()): ?>
