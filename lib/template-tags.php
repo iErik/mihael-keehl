46fd49618f9d325e58ca4b0246d05c409b2421d6
Erik Isidore <ericssonio@protonmail.ch>
2018-01-01 18:45:26 -0200

    Add missing template-tags.php module

---
+++ lib/template-tags.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Roots\Sage\Tags;
+
+
+if ( !function_exists('mihael_post_tags') ) :
+  /**
+   * Prints HTML with information for the post tags.
+   */
+  function mihael_post_tags() {
+    if ( 'post' == get_post_type() ) {
+
+      /* translators: used between list items, there is a space after the comma */
+      $tags_list = get_the_tag_list( '', __( ', ', 'mihael-keehl' ) );
+      if ( $tags_list ) {
+        printf( '<span class="post-tags">' . __( 'Tagged: %1$s', 'mihael-keehl' ) . '</span>', $tags_list );
+      }
+    }
+  }
+endif;
+
+if ( !function_exists('mihael_post_pagination') ) :
+  /**
+   * Prints the post's pagination links
+   */
+  function mihael_post_pagination() {
+    wp_link_pages(array(
+      'before' => '<div class="page-links pagination">',
+      'after'  => '</div>',
+      'next_or_number' => 'next'
+    ));
+  }
+endif;
